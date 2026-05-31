1275bd3706531a730bf61ec0603c9f1b5471c9e6
karuna-66 <yenumulakaruna@gmail.com>
2026-05-31 14:33:46 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+# Dataset paths
+train_dir = "dataset/train"
+test_dir = "dataset/test"
+
+# Image preprocessing
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='binary'
+)
+
+test_data = test_datagen.flow_from_directory(
+    test_dir,
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='binary'
+)
+
+# CNN Model
+model = Sequential([
+    Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(64, (3,3), activation='relu'),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(1, activation='sigmoid')
+])
+
+model.compile(
+    optimizer='adam',
+    loss='binary_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train model
+model.fit(
+    train_data,
+    epochs=10,
+    validation_data=test_data
+)
+
+# Save model
+model.save("fake_image_detector.h5")
+
+print("Model trained and saved successfully!")
